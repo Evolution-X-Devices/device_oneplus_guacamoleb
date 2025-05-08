5b2faa08dc4aaa551f665f04ea1d50599c48ed9d
Onelots <onelots@onelots.fr>
2025-05-08 14:39:01 +0200

    guacamoleb: overlay: add correct configs for fast charging
    Signed-off-by: Onelots <onelots@onelots.fr>

--- device.mk
+++ device.mk
@@ -30,6 +30,7 @@ TARGET_BOARD_FASTBOOT_INFO_FILE := $(LOCAL_PATH)/fastboot-info.txt
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-evolution \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
